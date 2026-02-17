357f7383c4911bb5599da4366f8af8d6815d1e9d
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-02-18 02:38:28 +0600

    email helper and jwt helper added

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,6 +9,13 @@ export default {
   database_url: process.env.DATABASE_URL,
   bcrypt_solt_round: Number(process.env.BCRYPT_SOLT_ROUND),
   cors_origin: process.env.CORS_ORIGIN,
+  email: {
+    from: process.env.EMAIL_FROM,
+    user: process.env.EMAIL_USER,
+    port: process.env.EMAIL_PORT,
+    host: process.env.EMAIL_HOST,
+    pass: process.env.EMAIL_PASS,
+  },
   admin: {
     name: process.env.NAME,
     email: process.env.EMAIL,
---
+++ src/helpers.ts/emailHelper.ts
@@ -0,0 +1,42 @@
+import nodemailer from "nodemailer";
+import config from "src/config/index.js";
+
+export type ISendEmail = {
+  to: string;
+  subject: string;
+  html: string;
+};
+
+const transporter = nodemailer.createTransport({
+  host: config.email.host,
+  port: Number(config.email.port),
+  secure: false,
+  auth: {
+    user: config.email.user,
+    pass: config.email.pass,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  logger: true,
+  debug: true,
+});
+
+const sendEmail = async (values: ISendEmail) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Simply Good Food" <${config.email.from}>`,
+      to: values.to,
+      subject: values.subject,
+      html: values.html,
+    });
+
+    console.log("Mail send successfully", info.accepted);
+  } catch (error) {
+    console.error("Email", error);
+  }
+};
+
+export const emailHelper = {
+  sendEmail,
+};
