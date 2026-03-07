87d6bd1585f17c1d8f14a8b9c7bc8b7a39e5f9d2
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-03-07 23:44:39 +0600

    updated code

--- src/app/modules/bike/bike.controller.ts
+++ src/app/modules/bike/bike.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import catchAsync from "src/app/shared/catchAsync.js";
-import { BikeService } from "./bike.services.js";
 import sendResponse from "src/app/shared/sendResponse.js";
+import { BikeService } from "./bike.services.js";
 
 const createBike = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
@@ -14,3 +14,45 @@ const createBike = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+
+const getBikeById = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await BikeService.getBikeById(id);
+
+  sendResponse(res, {
+    success: true,
+    message: "Bike retrieved successfully",
+    statusCode: 200,
+    data: result,
+  });
+});
+const updateBike = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  const { id } = req.params;
+  const result = await BikeService.updateBike(id, payload);
+
+  sendResponse(res, {
+    success: true,
+    message: "Bike updated successfully",
+    statusCode: 200,
+    data: result,
+  });
+});
+const deleteBike = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await BikeService.deleteBike(id);
+
+  sendResponse(res, {
+    success: true,
+    message: "Bike deleted successfully",
+    statusCode: 200,
+    data: result,
+  });
+});
+
+export const BikeController = {
+  createBike,
+  getBikeById,
+  updateBike,
+  deleteBike,
+};
