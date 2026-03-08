ffe0cbdb9ef0d7cc6a5bdb4d924e08488b6cb7e6
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-03-09 02:17:56 +0600

    blog route modified

--- src/app/modules/blog/blog.routes.ts
+++ src/app/modules/blog/blog.routes.ts
@@ -2,14 +2,29 @@ import express from "express";
 import { BlogController } from "./blog.controller.js";
 import auth from "src/app/middlewares/auth.js";
 import { Role } from "@prisma/client";
+import fileUploadHandler from "src/app/middlewares/fileUploadHandler.js";
+import validateRequest from "src/app/middlewares/validateRequest.js";
+import { CreateBlogSchema, UpdateBlogSchema } from "./blog.validation.js";
 
 const router = express.Router();
 
 router.get("/", BlogController.getAllBlogs);
-router.post("/", auth(Role.ADMIN), BlogController.createBlog);
+router.post(
+  "/",
+  fileUploadHandler(),
+  auth(Role.ADMIN),
+  validateRequest(CreateBlogSchema),
+  BlogController.createBlog,
+);
 router.get("/:id", BlogController.getBlogById);
 router.get("/:slug", BlogController.getBlogBySlug);
-router.patch("/:id", auth(Role.ADMIN), BlogController.updateBlog);
+router.patch(
+  "/:id",
+  fileUploadHandler(),
+  auth(Role.ADMIN),
+  validateRequest(UpdateBlogSchema),
+  BlogController.updateBlog,
+);
 router.delete("/:id", auth(Role.ADMIN), BlogController.deleteBlog);
 
 export const BlogRouter = router;
