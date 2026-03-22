d0aaaef2067ef88819ba6df8ad0a2fa4845a7acc
mohammad-anar <ai.anarul4232@gmail.com>
2026-03-22 12:16:42 +0600

    .js files removed

--- src/helpers.ts/jwtHelper.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-const createToken = (payload, secret, expireTime) => {
-    return jwt.sign(payload, secret, { expiresIn: expireTime });
-};
-const verifyToken = (token, secret) => {
-    return jwt.verify(token, secret);
-};
-export const jwtHelper = { createToken, verifyToken };
